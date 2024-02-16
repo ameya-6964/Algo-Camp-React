f4bd0c2c9e0ce02ba4834d32fd668a156ae3e332
ameya-6964 <belvalkar7@gmail.com>
2024-02-16 23:45:15 +0530

    Added Local State Variable Value With Help Of UseState Hook

--- Lecture_4_Memory_Of_Component/src/Components/Counter.jsx
+++ Lecture_4_Memory_Of_Component/src/Components/Counter.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const [value, setValue] = useState(1);
+  function addClickHandler() {
+    setValue((value) => value + 1);
+  }
+  function substractClickHandler() {
+    if (value === 0) {
+      return;
+    }
+    setValue((value) => value - 1);
+  }
   return (
     <>
       <div className="counter">
-        <h1>10</h1>
+        <h1>{value}</h1>
       </div>
       <div className="buttons">
-        <button className="add">Add</button>
-        <button className="substract">Substract</button>
+        <button onClick={addClickHandler} className="add">
+          Add
+        </button>
+        <button onClick={substractClickHandler} className="substract">
+          Substract
+        </button>
       </div>
     </>
   );
